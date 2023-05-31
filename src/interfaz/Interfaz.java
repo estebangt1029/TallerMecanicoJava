40e42ab10a7c3b87c565b07d92c5e32e68c7f5dc
estebangt1029 <1029estebangt@gmail.com>
2023-05-30 20:32:14 -0500

    first commit

---
+++ src/interfaz/Interfaz.java
@@ -0,0 +1,11 @@
+
+package interfaz;
+
+public class Interfaz {
+
+    public static void main(String[] args) {
+        JFramePrincipal P=new JFramePrincipal();
+        P.setVisible(true);
+    }
+    
+}
